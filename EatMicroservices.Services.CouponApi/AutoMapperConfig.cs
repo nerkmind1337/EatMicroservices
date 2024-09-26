eaf36b4654c8604e75377f593caf122101bbfbb8
lewis clarke <boolie205@gmail.com>
2024-09-26 17:39:23 +0200

    Init of project and progress on coupon api

---
+++ EatMicroservices.Services.CouponApi/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EatMicroservices.Services.CouponApi.Models;
+using EatMicroservices.Services.CouponApi.Models.Dto;
+namespace EatMicroservices.Services.CouponApi
+{
+    public class AutoMapperConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ EatMicroservices.Services.CouponApi/Controllers/CouponApiController.cs
@@ -0,0 +1,59 @@
+﻿using EatMicroservices.Services.CouponApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EatMicroservices.Services.CouponApi.Models;
+using EatMicroservices.Services.CouponApi.Models.Dto;
+using AutoMapper;
+using static Azure.Core.HttpHeader;
+namespace EatMicroservices.Services.CouponApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly ResponseDto _response;
+        private IMapper _mapper;
+        
+        public CouponApiController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> couponList = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(couponList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(record => record.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
+
